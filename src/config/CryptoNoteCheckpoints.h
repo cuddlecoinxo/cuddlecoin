17f9d8f47228a5880a7572e212acc56f3177c69c
adam's github website <paper.adam@gmail.com>
2021-05-05 15:33:16 +1000

    Update CryptoNoteCheckpoints.h

--- src/config/CryptoNoteCheckpoints.h
+++ src/config/CryptoNoteCheckpoints.h
@@ -18,8 +18,6 @@ namespace CryptoNote
         const char *blockId;
     };
 
-    const std::initializer_list<CheckpointData> CHECKPOINTS = {
-  { 10000, "84b6345731e2702cdaadc6ce5e5238c4ca5ecf48e3447136b2ed829b8a95f3ad"}
-};
+ const std::initializer_list<CheckpointData> CHECKPOINTS = {};
       
 } // namespace CryptoNote
