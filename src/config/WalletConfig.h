f5dc04f316925b811192d7983b00de007f20e99a
cuddlecoinxo <83870593+cuddlecoinxo@users.noreply.github.com>
2021-06-07 23:50:08 +1000

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -13,7 +13,7 @@ namespace WalletConfig
     const std::string_view addressPrefix = "hugs";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
-    const std::string ticker = "hugs";
+    const std::string ticker = "CUDL";
 
     /* The filename to output the CSV to in save_csv */
     const std::string csvFilename = "transactions.csv";
