63abbad6b2782fa522f9b26f6e330936fe6e9ef7
cuddlecoinxo <83870593+cuddlecoinxo@users.noreply.github.com>
2021-06-12 14:50:03 +1000

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -2,7 +2,7 @@
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2018, The TurtleCoin Developers
 // Copyright (c) 2019, 2ACoin Developers
-// Copyright (c) 2019, The CuddleCoin Developers
+// Copyright (c) 2021, The CuddleCoin Developers
 //
 // Please see the included LICENSE file for more information.
 
